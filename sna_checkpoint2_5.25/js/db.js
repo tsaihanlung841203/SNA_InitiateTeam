07e40a9a11c9d4abb1c616a2655ecca4839f7fb6
mark11028 <mark11028@gmail.com>
2017-06-07 16:46:01 +0800

    Web ui update

--- sna_checkpoint2_5.25/js/db.js
+++ sna_checkpoint2_5.25/js/db.js
@@ -43,3 +43,26 @@ $("#btn2").click(function(){
         }
     });
 });
+var filechooser = document.getElementById('filechooser');
+var previewer = document.getElementById('previewer');
+
+filechooser.onchange = function() {
+    var files = this.files;
+    var file = files[0];
+
+    // 接受 jpeg, jpg, png 类型的图片
+    if (!/\/(?:jpeg|jpg|png)/i.test(file.type)) return;
+
+    var reader = new FileReader();
+
+    reader.onload = function() {
+        var result = this.result;
+
+        previewer.src = result;
+
+        // 清空图片上传框的值
+        filechooser.value = '';
+    };
+
+    reader.readAsDataURL(file);
+};
